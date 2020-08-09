707ee43678ae9f822eb5dc027281d557b4bec404
colinlcrawford <ccolin84@gmail.com>
2020-08-09 16:59:30 -0400

    add bash script to ease starting with cron job

--- bin/eleetcoach.rb
+++ bin/eleetcoach.rb
@@ -4,7 +4,16 @@ Dir[File.join(__dir__, '..', 'app', '*.rb')].each { |file| require file }
 
 logger = Logger.new(STDOUT)
 
-config = Config.new(logger: logger)
+config = nil
+config_file_flag_inx = ARGV.find_index("--config")
+
+if !config_file_flag_inx.nil?
+  config_file = ARGV[config_file_flag_inx + 1]
+  config = Config.new(logger: logger, config_file: config_file)
+else
+  config = Config.new(logger: logger)
+end
+
 leetcode = Leetcode::Client.new(logger: logger)
 mailer = GMail.new(
   logger: logger,
